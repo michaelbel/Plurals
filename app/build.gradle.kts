f83f445e516289770c9021d80dadc8fef710a7cf
michaelbel <michael-bel@outlook.com>
2026-01-23 21:50:45 +0300

    Update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.nio.charset.StandardCharsets
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
